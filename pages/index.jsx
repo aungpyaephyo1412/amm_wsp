a2ab2bacf07c3f5527e6336b1b724bbdacec5b89
aungpyaephyo1412 <aungpyaephyo1412@gmail.com>
2023-09-19 09:33:14 +0630

    day 3

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,12 +1,30 @@
-import {useUserName} from "@/context/userNameContext";
+import axios from "axios";
+import {useEffect, useState} from "react";
+
 const Index = () => {
-    const name = useUserName();
-    console.log(name)
+    const url = "https://jsonplaceholder.typicode.com/todos"
+    const [todo,setTodo] = useState([])
+    useEffect(() => {
+        axios.get(url)
+            .then(res=>{
+                console.log(res.data)
+                setTodo(res.data)
+            })
+            .catch(error=>{
+                console.log(error.message)
+            })
+    }, []);
     return (
         <div>
-          Hello World
-            <div>
-                    About
+            <div className='container mx-auto p-3 grid grid-cols-2 md:grid-cols-3 lg:grid-cols-5 gap-3'>
+                {
+                    todo?.map(todo=>(
+                        <div key={todo.id} className="block max-w-sm p-6 bg-white border border-gray-200 rounded-lg shadow hover:bg-gray-100">
+                            <h5 className="mb-2 text-xl font-bold tracking-tight text-gray-900 line-clamp-2">{todo.title}</h5>
+                        </div>
+
+                    ))
+                }
             </div>
         </div>
     );
