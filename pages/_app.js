eeecb8f6a2c9a7bb0f1728504503b25aaf3a2d93
aungpyaephyo1412 <aungpyaephyo1412@gmail.com>
2023-09-19 10:41:47 +0630

    react query

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,16 @@
 import '@/styles/globals.css'
+import {QueryClient, QueryClientProvider} from "@tanstack/react-query";
 
-export default function App({ Component, pageProps }) {
-  return (
+const queryClient = new QueryClient({
+    defaultOptions: {
+        queries: {
+            refetchOnWindowFocus: false, // default: true
+            staleTime: 1000 * 60 * 60 * 24
+        },
+    },
+})
+export default function App({Component, pageProps}) {
+    return (<QueryClientProvider client={queryClient}>
             <Component {...pageProps} />
-  )
+        </QueryClientProvider>)
 }
--- pages/index.jsx
+++ pages/index.jsx
@@ -1,12 +1,27 @@
-import {useUserName} from "@/context/userNameContext";
+import axios from "axios";
+import {useQuery} from "@tanstack/react-query";
+
 const Index = () => {
-    const name = useUserName();
-    console.log(name)
+    const getAllTodo = async ()=>{
+        const res = await axios.get("https://jsonplaceholder.typicode.com/todos")
+        return res.data;
+    }
+    const {
+        data:todos,
+        isLoading,
+        isError,
+        isSuccess
+    } = useQuery({ queryKey: ['get','todos'], queryFn: getAllTodo })
+    console.log(isLoading,isError,isSuccess)
     return (
         <div>
           Hello World
             <div>
-                    About
+                {
+                    isSuccess && todos.map(todo=>{
+                        return <li key={todo.id}>{todo.id} - {todo.title}</li>
+                    })
+                }
             </div>
         </div>
     );
